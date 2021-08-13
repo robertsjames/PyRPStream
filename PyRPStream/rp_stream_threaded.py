b9637cc7b9809e062462aab73cc0ea00b24ea296
Robert James <robert.james.19@ucl.ac.uk>
2021-08-13 14:08:32 +0100

    Add in RP streaming files from private repo.

---
+++ PyRPStream/rp_stream_threaded.py
@@ -0,0 +1,266 @@
+"""
+08/21, R James, F Alder
+
+Adapted from script by https://github.com/awmlee
+"""
+
+import socket
+import threading
+import queue
+import numpy as np
+import time
+
+class ClientCommand:
+    """ A command to the client thread.
+        Each command key has its associated command type:
+
+        'CONNECT':    (host, port) tuple
+        'RECEIVE':    None
+        'CLOSE':      None
+    """
+
+    def __init__(self, key, command=None):
+        self.key = key
+        self.command = command
+
+
+class ClientReply:
+    """ A reply from the client thread.
+        Each reply key has its associated reply type:
+
+        'ERROR':      The error string
+        'DATA':       Depends on the command - for RECEIVE it's the received
+                      data string, for others None
+        'MESSAGE':    Status message
+    """
+
+    def __init__(self, key, reply=None):
+        self.key = key
+        self.reply = reply
+
+
+class SocketClientThread(threading.Thread):
+    """ Implements the threading (run, join, etc.): the thread can be
+        controlled via the cmd_q queue attribute. Replies are placed in
+        the reply_q queue attribute.
+    """
+    def __init__(self):
+        super(SocketClientThread, self).__init__()
+
+        # Command and reply queues for communicating with the socket thread
+        self.cmd_q = queue.Queue()
+        self.reply_q = queue.Queue()
+        # Thread run control
+        self.alive = threading.Event()
+        self.alive.set()
+        # Socket connection attributes
+        self.connected = 0
+        self.socket = None
+        # Buffer reading attributes
+        self.header_size = 60
+        self.ch1_size = 32768
+        self.ch2_size = 32768
+        # Data storage
+        self.data_all_ch1 = []
+        self.data_all_ch2 = []
+        # Thread control handlers
+        self.handlers = {
+            'CONNECT': self._handle_CONNECT,
+            'RECEIVE': self._handle_RECEIVE,
+            'CLOSE': self._handle_CLOSE
+        }
+
+        print('Starting socket thread')
+
+
+    def run(self):
+        """ Thread control function.
+        """
+        while self.alive.isSet():
+            if self.connected:
+                # Default is to RECEIVE
+                command = ClientCommand('RECEIVE')
+                self.handlers[command.key](command)
+            else:
+                # Don't do anything if not connected
+                time.sleep(0.1)
+            try:
+                # Check if any commands have been sent to the thread
+                command = self.cmd_q.get(block=False)
+                self.handlers[command.key](command)
+            except queue.Empty:
+                # Do nothing if no commands sent
+                continue
+
+    def join(self, timeout=None):
+        """ Invoking this will end the thread.
+        """
+        self.alive.clear()
+        threading.Thread.join(self, timeout)
+
+    def _handle_CONNECT(self, client_command):
+        """ Attempt to connect to the socket, if not already connected
+        client_command should be a (host, port) tuple.
+        """
+        try:
+            if self.connected == 0:
+                # Attempt to connect if not already connected
+                self.socket = socket.socket(
+                    socket.AF_INET, socket.SOCK_STREAM)
+                self.socket.connect((client_command.command[0], client_command.command[1]))
+                self.reply_q.put(self._message_reply('Socket connected'))
+                self.connected = 1
+            else:
+                # We are already connected, do nothing
+                self.reply_q.put(self._message_reply('Socket already connected'))
+        except OSError as e:
+            # Return error if we can't connect
+            self.reply_q.put(self._error_reply(str(e)))
+            self.connected = 0
+
+    def _handle_RECEIVE(self, client_command):
+        """ Read from the socket: discard the header information, then send
+        data from channel 1 and channel 2 to the reply queue.
+        """
+        try:
+            reply = {}
+
+            # Take the header information from the socket, then discard
+            header_bytes = self._receieve_bytes(self.header_size, self.socket)
+
+            # Take channel 1 data from the socket, store in reply
+            ch1_bytes = self._receieve_bytes(self.ch1_size, self.socket)
+            reply.update({'ch1_data': ch1_bytes})
+
+            # Take channel 2 data from the socket, store in reply
+            ch2_bytes = self._receieve_bytes(self.ch2_size, self.socket)
+            reply.update({'ch2_data': ch2_bytes})
+
+            # Store the timestamp in reply
+            reply.update({'timestamp': time.time()})
+
+            # Put reply in the reply queue
+            self.reply_q.put(self._data_reply(reply), block=True)
+
+        except OSError as e:
+            self.reply_q.put(self._error_reply(str(e)))
+            self.connected = 0
+
+    def _receieve_bytes(self, n, socket):
+        """ Receive n bytes from socket.
+        """
+        tdata = bytearray()
+        while len(tdata) < n:
+            packet = socket.recv(n - len(tdata))
+            if not packet:
+                raise OSError('Empty packet from socket.recv()')
+            tdata.extend(packet)
+
+        return tdata
+
+    def _handle_CLOSE(self, client_command):
+        """ Close connection to the socket.
+        """
+        self.socket.close()
+        self.reply_q.queue.clear()
+        self.reply_q.put(self._message_reply('Socket closed'))
+        self.connected = 0
+
+    def _error_reply(self, errstr):
+        return ClientReply('ERROR', errstr)
+
+    def _message_reply(self, data=None):
+        return ClientReply('MESSAGE', data)
+
+    def _data_reply(self, data=None):
+        return ClientReply('DATA', data)
+
+
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+    try:
+        acq_time = float(input('Acquisition time in seconds:'))
+        assert(acq_time > 0)
+    except:
+        raise TypeError('Invalid acquisition time value')
+
+    # Number of messages to use for data rate calculation
+    n_samp = 10
+
+    # Connection information
+    SERVER_ADDR = 'rp-f05a98.local', 8900
+
+    # Start the socket thread
+    client = SocketClientThread()
+    client.start()
+
+    # Attempt to CONNECT to the socket
+    print('Trying to CONNECT to socket')
+    # Make 10 attempts, then give up
+    for i in range(10):
+        client.cmd_q.put(ClientCommand('CONNECT', SERVER_ADDR))
+        client_reply = client.reply_q.get()
+        print(client_reply.reply)
+        if client_reply.key == 'ERROR':
+            print('Trying to CONNECT again')
+        else:
+            break
+
+    # Check if we are connected by attempting to RECEIVE
+    client.cmd_q.put(ClientCommand('RECEIVE'))
+    client_reply = client.reply_q.get()
+    if client_reply.key == 'ERROR':
+        # We aren't connected: exit
+        client.join()
+        raise OSError('Repeatedly failed to execute CONNECT: exiting')
+    else:
+        # We are connected: continue
+        print('Starting main acquisition')
+
+    time.sleep(1)
+
+    t_start = time.time()
+    t_prev = t_start
+    i = 0
+
+    # Loop to RECEIVE DATA, unless an ERROR occurs
+    while 1:
+        # Get reply from reply queue
+        client_reply = client.reply_q.get()
+
+        if client_reply.key == 'ERROR':
+            # Exit if we receive ERROR
+            print(client_reply.reply)
+            client.join()
+            raise OSError('Error during RECEIVE: exiting')
+
+        elif client_reply.key == 'DATA':
+            # If we have DATA, exit if we have exceeded acquisition time
+            t = client_reply.reply['timestamp']
+            if t - t_start > acq_time:
+                break
+
+            # Otherwise, save the data to files
+            np.savetxt(f'red_pitaya_data_ch1_{i}.txt', np.frombuffer(client_reply.reply['ch1_data'], dtype=np.int16))
+            np.savetxt(f'red_pitaya_data_ch2_{i}.txt', np.frombuffer(client_reply.reply['ch2_data'], dtype=np.int16))
+
+            # Calculate the data rate every n_samp messages
+            if i == n_samp:
+                print('Data rate is ', n_samp * 2**16 / (t - t_prev) / 1024 / 1024)  # MBytes/second
+                i = 0
+                t_prev = t
+
+            i += 1
+
+    # CLOSE the socket after a successful acquisition run
+    client.cmd_q.put(ClientCommand('CLOSE'))
+
+    time.sleep(1)
+
+    # Get reply upon socket closure
+    client_reply = client.reply_q.get()
+    print(client_reply.reply)
+
+    # End the thread to exit the program
+    client.join()
+    print('Data acquisition successful: exiting')
---
+++ PyRPStream/start_streamserver_websocket.py
@@ -0,0 +1,57 @@
+"""
+08/21, R James, F Alder
+
+Adapted from script by https://github.com/awmlee
+"""
+import time
+from websocket import create_connection
+import requests
+import json
+import keyboard
+
+class Websocket:
+    def __init__(self, socket_url=None):
+        self.rp_ip = 'rp-f05a98.local' if socket_url == '' else socket_url
+        self.socket_url = 'ws://' + self.rp_ip + '/wss'
+        self.start_app_url = 'http://' + self.rp_ip + '/bazaar?start=streaming_manager'
+        self.stop_app_url = 'http://' + self.rp_ip + '/bazaar?stop=streaming_manager'
+
+
+#------------------------------------------------------------------------------
+if __name__ == '__main__':
+    socket_url = input('Red Pitaya IP address (press enter for default: rp-f05a98.local):')
+    WS = Websocket(socket_url)
+
+    # Start streaming server
+    try:
+        web = requests.get(WS.start_app_url)
+        print('Connection established')
+    except:
+        raise OSError('Could not connect to Red Pitaya')
+
+    # Create websocket connection
+    ws = create_connection(WS.socket_url)
+
+    # Set websocket parameters
+    setbits = ({'parameters': {'SS_RESOLUTION': {'value': 2}, 'in_command': {'value': 'send_all_params'}}})
+    setrate = ({'parameters': {'SS_RATE': {'value': 800}, 'in_command': {'value': 'send_all_params'}}})
+    setdualchan = ({'parameters': {'SS_CHANNEL': {'value': 3},' in_command': {'value': 'send_all_params'}}})
+    ws.send(json.dumps(setbits))
+    ws.send(json.dumps(setrate))
+    ws.send(json.dumps(setdualchan))
+
+    startcmd = ({'parameters': {'SS_START': {'value': 1}, 'in_command': {'value': 'send_all_params'}}})
+    stopcmd = ({'parameters': {'SS_START': {'value': 0}, 'in_command': {'value': 'send_all_params'}}})
+
+    # Start streaming data
+    ws.send(json.dumps(startcmd))
+
+    print('Streaming: press q to stop')
+    while True:
+        if keyboard.read_key() == 'q':
+            print('Ending streaming, closing connection')
+            break
+
+    # Stop streaming data and close websocket
+    ws.send(json.dumps(stopcmd))
+    ws.close()
