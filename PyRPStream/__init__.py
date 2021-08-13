7eb89c8c328c2720bbbce57a294de1059e79bea1
Robert James <robert.james.19@ucl.ac.uk>
2021-08-13 13:36:14 +0100

    Basic tools to pip install.

---
+++ PyRPStream/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.0'
+__author__ = 'Robert James, Fiona Alder'
+__author_email__ = 'robert.james.19@ucl.ac.uk'
---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requires = [x.strip() for x in f.readlines()]
+
+setuptools.setup(
+    name='PyRPStream',
+    version='0.1.0',
+    author='Robert James, Fiona Alder',
+    author_email='robert.james.19@ucl.ac.uk',
+    url='https://github.com/robertsjames/PyRPStream',
+    python_requires=">=3.8",
+    install_requires=requires,
+    packages=setuptools.find_packages())
